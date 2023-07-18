0b1647daeedbf39562e830cffad12cf81c4279a7
kkelly27 <70247427+kkelly27@users.noreply.github.com>
2023-07-18 00:55:31 -0400

    Add files via upload

---
+++ DOM Exercise/DOM Exercise.js
@@ -0,0 +1,12 @@
+1 document.getElementById("container");
+2 document.querySelector("#container")
+3 document.getElementByClassName("second") 
+4 document.querySelector("ol .third");
+5 let foundDiv = document.querySelector("#containter"); 
+footer.className += "main";
+footer.classList.remove("main");
+let newLis = document.createElement("li");
+newLis.innerText = "four"
+FileList.appendChild(newLi);
+let list = document.querySelectorAll("ol li");
+footer.remove();
